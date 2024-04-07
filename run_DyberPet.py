d14098295cbffc887d06eefd137c618ae88ae77a
admin <admin@users.noreply.github.com>
2024-04-08 00:26:22 +0800

    refine main

--- run_DyberPet.py
+++ run_DyberPet.py
@@ -53,34 +53,24 @@ class DyberPetApp(QApplication):
 
     def __init__(self, *args, **kwargs):
         super(DyberPetApp, self).__init__(*args, **kwargs)
-
         # Connect the signal to a slot
         self.setStyleSheet(StyleSheet)
-
         self.setQuitOnLastWindowClosed(False)
         screens = self.screens()
-
         # internationalization
         fluentTranslator = FluentTranslator(QLocale(settings.language_code))
         self.installTranslator(fluentTranslator)
         self.installTranslator(settings.translator)
-        # setThemeColor(settings.THEME_COLOR)
-
         # Pet Object
         self.p = PetWidget(screens=screens)
-
         # Notification System
         self.note = DPNote()
-
         # Accessory System
         self.acc = DPAccessory()
-
         # System Panel
         self.conp = ControlMainWindow()
-
         # Dashboard
         self.board = DashboardMainWindow()
-
         # Signal Links
         self.__connectSignalToSlot()
 
@@ -94,21 +84,15 @@ class DyberPetApp(QApplication):
         self.p.setup_acc.connect(self.acc.setup_accessory)
         self.p.move_sig.connect(self.acc.send_main_movement)
         self.p.close_all_accs.connect(self.acc.closeAll)
-
         # System Widgets - others
         self.conp.settingInterface.ontop_changed.connect(self.acc.ontop_changed)
         self.conp.settingInterface.scale_changed.connect(self.acc.reset_size_sig)
-
         self.conp.settingInterface.ontop_changed.connect(self.p.ontop_update)
-        # self.conp.settingInterface.scale_changed.connect(self.p.set_img)
         self.conp.settingInterface.scale_changed.connect(self.p.reset_size)
         self.conp.settingInterface.lang_changed.connect(self.p.lang_changed)
-
         self.conp.charCardInterface.change_pet.connect(self.p._change_pet)
         self.p.show_controlPanel.connect(self.conp.show_window)
-
         self.conp.gamesaveInterface.refresh_pet.connect(self.p.refresh_pet)
-
         # Dashboard - others
         self.p.show_dashboard.connect(self.board.show_window)
         self.note.noteToLog.connect(self.board.statusInterface._addNote)
@@ -117,7 +101,6 @@ class DyberPetApp(QApplication):
         self.p.change_note.connect(self.board.statusInterface._changePet)
         self.board.statusInterface.changeStatus.connect(self.p._change_status)
         self.p.stopAllThread.connect(self.board.statusInterface.stopBuffThread)
-
         self.acc.acc_withdrawed.connect(self.board.backpackInterface.acc_withdrawed)
         self.board.backpackInterface.use_item_inven.connect(self.p.use_item)
         self.board.backpackInterface.item_note.connect(self.p.register_notification)
@@ -131,7 +114,6 @@ class DyberPetApp(QApplication):
         self.p.refresh_bag.connect(self.board.backpackInterface.refresh_bag)
         self.p.refresh_bag.connect(self.board.shopInterface.refresh_shop)
         self.p.addCoins.connect(self.board.backpackInterface.addCoins)
-
         # Tasks and Timer
         self.board.taskInterface.focusPanel.start_pomodoro.connect(self.p.run_tomato)
         self.board.taskInterface.focusPanel.cancel_pomodoro.connect(
@@ -154,7 +136,6 @@ else:
     basedir = os.path.dirname(__file__)
 
 if __name__ == "__main__":
-
     # Avoid multiple process
     try:
         me = singleton.SingleInstance()
@@ -165,9 +146,6 @@ if __name__ == "__main__":
     QApplication.setHighDpiScaleFactorRoundingPolicy(
         Qt.HighDpiScaleFactorRoundingPolicy.PassThrough
     )
-    # QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-    # QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps)
-
     app = DyberPetApp(sys.argv)
     app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
 
